9f047096df282011cc2e8aad742ef4e395f4f602
SangBejo1 <ayubsubagiya@gmail.com>
2022-04-07 16:13:02 +0700

    Biodata

---
+++ src/main/java/com/mycompany/biodataayubss/BIODATAAYUBSS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.biodataayubss;
+
+/**
+ *
+ * @author HP
+ */
+public class BIODATAAYUBSS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Biodatanya Biodata = new Biodatanya();
+        Biodata.Nama_Lengkap();
+        Biodata.Tempat_Lahir();
+        Biodata.Tanggal_Lahir();
+        Biodata.Hobi();
+        Biodata.Zodiak();
+        Biodata.Makanan_Favorit();
+        Biodata.Cita_Cita();
+    }
+}
---
+++ src/main/java/com/mycompany/biodataayubss/Biodatanya.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.biodataayubss;
+
+/**
+ *
+ * @author HP
+ */
+public class Biodatanya {
+    String namaDepan = "Ayub ";
+    String namaBelakang = "Subagiya";
+    String tempatLahir = "Jakarta";
+    String tanggalLahir = "02 Juli 2003";
+    String hobi = "Bermain Futsal";
+    String zodiak = "Virgo";
+    String makananFavorit = "Seafood";
+    String cita_cita = "Sukses Dunia Akhirat";
+    
+    void Nama_Lengkap() {
+        System.out.println(namaDepan+namaBelakang);
+    }
+    void Tempat_Lahir() {
+        System.out.println(tempatLahir);
+    }
+    void Tanggal_Lahir() {
+        System.out.println(tanggalLahir);
+    }
+    void Hobi() {
+        System.out.println(hobi);
+    }
+    void Zodiak() {
+        System.out.println(zodiak);
+    }
+    void Makanan_Favorit() {
+        System.out.println(makananFavorit);
+    }
+    void Cita_Cita() {
+        System.out.println(cita_cita);
+    }
+    
+}
